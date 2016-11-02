bd4020cf5d97d5cbdc376bde9a29dd04b935ecf0
Jeff Shomali <jeffshomali@gmail.com>
2016-11-01 18:53:13 -0700

    Working Widget Class

--- contact-widget/contact-widget.php
+++ contact-widget/contact-widget.php
@@ -1,33 +1,30 @@
 <?php
-/*
-Plugin Name: Ajax Contact Widget
-Description: Simple Ajax Contact Form Widget
-Version:     20160911
-Author:      Jeff Shomali
-Author URI:  http://jeffcv.tk
-License:     GPL2
-*/
-
 /**
- * Include Javascript
+ * Plugin Name: Ajax Contact Widget
+ * Description: Simple Ajax powered contact form widget
+ * Version: 1.0
+ * Author: Jeff Shomali
+ * Author URI: http://www.jeffcv.tk
+ * License: GPL2
  */
-function add_scripts(){
-     wp_enqueue_scripts('contact-scripts', plugins_url().'/contact-widget/js/script.js',array('jquery'), '1.0.0', false);
-}
 
-add_action('wp_enqueue_scripts', 'add_scripts'); //load javascript files
+ /*
+  *	Include JavaScript
+  */
+  function add_scripts(){
+	wp_enqueue_script('contact-scripts', plugins_url().'/contact-widget/js/script.js', array('jquery'),'1.0.0', false);
+  }
+  add_action('wp_enqueue_scripts','add_scripts');
 
-/**
- *  Include Class
- */
-include('class.contact-widget.php');
+  /*
+   *	Include Class
+   */
+   include('class.contact-widget.php');
 
-/**
- *  Register The Widget
- */
-function register_contact_widget(){
-     register_widget('Contact_widget');
-}
-
-//Hooks a function on to a specific action.
-add_action('widgets_init', 'egister_contact_widget');
+   /*
+   *	Register Widget
+   */
+   function register_contact_widget(){
+	register_widget('Contact_Widget');
+   }
+   add_action('widgets_init','register_contact_widget');
