ea696047eca18c2d665d3ce347ef246d250e866d
Jeff Shomali <jeffshomali@gmail.com>
2016-11-01 19:13:26 -0700

    Submitting Contact Form With JQuery and Ajax

---
+++ contact-widget/js/script.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function(){
+     //Get Form
+     var form = jQuery('#ajax-contact');
+
+     //Message
+     var formMessages = jQuery('#form-messages');
+
+     //Form Event Handler
+     jQuery(form).submit(function(event){
+          //stop browser from submitting form directly
+          event.preventDefault();
+          console.log('Contact Form Submitted');
+
+          //Serliaized Data
+          var formData = jQuery(form).serialize();
+
+          //Submit with ajax
+          jQuery.ajax({
+               type: 'post',
+               url: jQuery(form).attr('action'),
+               data : formData
+
+          }).done(function(response){
+               //Make sure sent was success
+               jQuery(formMessages).removeClass('error');
+               jQuery(formMessages).addClass('error');
+
+               //Set Message Text
+               jQuery(formMessages).text(response);
+
+               //Clear the form inputs
+               jQuery('#name').val('');
+               jQuery('#email').val('');
+               jQuery('#message').val('');
+          }).fail(function(data){
+               //Make sure sent was not successfull
+               jQuery(formMessages).removeClass('error');
+               jQuery(formMessages).addClass('error');
+          });
+     });
+});
